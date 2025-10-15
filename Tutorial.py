b1c23394a0e3a168ace47eea7f734c5463a60676
Curro <82005658+Curro-47@users.noreply.github.com>
2025-10-15 11:06:16 -0600

    Start

---
+++ Tutorial.py
@@ -0,0 +1,80 @@
+## THIS IS A RECAP OF ALL INFORMATION OF ITESM Sem.3 2025-Agu-Dec PYTHON CLASS
+
+
+
+
+######## CODING BASICS #######################################################################################
+
+# - Starting a line with "#" turns it into a comment, the code ignores these lines and allows you to make notes
+
+# - Code runs line by line from top to bottom
+
+
+
+
+######## BASIC FUNCTIONS ############################################################################################
+
+# Print: Outputs text to the terminal (Shows it in the screen)
+print("Hello world")
+# It is written like "print()" and inside the parenthesis can be a string (text), or a variable
+
+# Input: Asks the user for text
+input("Input text: ")
+
+
+
+######## VARIABLES, DATA TYPES AND OPERATORS ############################################################################3
+
+######## Variables
+
+# Variables are used to store information
+# You create one by writing the name of the variable followed by "=" and the value you want to set to it
+variableName = 0
+hello = 1203.120
+howareyou12 = "HI"
+# These are all valid variables
+
+######## Data types
+
+# Variables can have different types, that is: text, number, etc...
+# The simplified list (Using variables as examples) is:
+int = 123         # Integer numbers
+float = 1.2942    # Decimal numbers
+string = "Hello"  # Text
+boolean = True    # True or false
+
+######## Operators
+#### You can use operations to affect each of the data types
+
+# Number operators (int and float)
+sum = 1 + 2
+substraction = 1 - 2
+multiplication = 1 * 2
+division = 1 / 2
+modulus = 1 % 2 # This gives the residual of its division, for example in 1/2 the residual is 1 so 1%2 = 1
+
+# Text Operators (string)
+concatenation = "Hello " + "World" # Combines 2 texts, the result here is "Hello World"
+
+# Boolean Operators (bool)
+bool_and = True and False # bool_and will be true if both values are true
+bool_or = True or False # bool_or will be true if any value is true
+
+# Comparator (also boolean operators)
+equals = 1 == 1 # if the values are equal the variable will be true
+equals = "Hello" == "Hello"
+equals = False == False
+
+not_equals = 1 != 0 # if the values are not equal the variable will be true
+not_equals = "Hello" != "Goodbye"
+not_equals = False != True
+
+greater_than = 1 > 0
+greater_or_equal_than = 1 >= 0
+less_or_equal_than = 0 <= 1
+less_than = 0 < 1
+
+
+
+
+########
